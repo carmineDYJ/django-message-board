3bf5841c70ef20286817b274a9c711182b80a7ef
carmineding <carmineding@tencent.com>
2021-12-22 20:08:13 +0800

    get all messages in index function

--- message_board/views.py
+++ message_board/views.py
@@ -7,12 +7,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     form = MessageForm(request.POST)
+    all_messages = list(Message.objects.all())
+    print(all_messages)
     context = {
         'form': form
     }
     if request.method == 'POST':
         if form.is_valid():
             Message.objects.create(name=form.cleaned_data['name'], body=form.cleaned_data['body'])
+            # reset form to blank
             form = MessageForm()
             context = {
                 'form': form
