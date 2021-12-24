1915d315fc44c2672e1d0b0c796443cadce99d61
carmineding <carmineding@tencent.com>
2021-12-24 17:48:19 +0800

    add min length limit in form

--- message_board/forms.py
+++ message_board/forms.py
@@ -1,6 +1,20 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class MessageForm(forms.Form):
-    name = forms.CharField(label='name', max_length=20, required=False)
-    body = forms.CharField(label='message', max_length=200, required=False, widget=forms.Textarea)
+    name = forms.CharField(label='name', max_length=20, min_length=1, required=True)
+    body = forms.CharField(label='message', max_length=200,  min_length=1, required=True, widget=forms.Textarea)
+    # def clean_name(self):
+    #     name = self.cleaned_data['name']
+    #     name = name.strip()
+    #     if len(name) == 0:
+    #         raise ValidationError('This field is required.')
+    #     return name
+    #
+    # def clean_body(self):
+    #     body = self.cleaned_data['body']
+    #     body = body.strip()
+    #     if len(body) == 0:
+    #         raise ValidationError('This field is required.')
+    #     return body
--- message_board/views.py
+++ message_board/views.py
@@ -7,10 +7,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     form = MessageForm(request.POST)
-    all_messages = list(Message.objects.all())
-    print(all_messages)
+    message_list = Message.objects.values()
+    print(message_list)
     context = {
-        'form': form
+        'form': form,
+        'message_list': message_list
     }
     if request.method == 'POST':
         if form.is_valid():
@@ -18,7 +19,18 @@ def index(request):
             # reset form to blank
             form = MessageForm()
             context = {
-                'form': form
+                'form': form,
+                'message_list': message_list
             }
             return render(request, 'message_board/index.html', context)
+        else:
+            for value in form.errors.get_json_data().values():
+                error_message = value[0]['message']
+            context = {
+                'form': form,
+                'message_list': message_list,
+                'error_message': error_message
+            }
+            return render(request, 'message_board/index.html', context)
+
     return render(request, 'message_board/index.html', context)
