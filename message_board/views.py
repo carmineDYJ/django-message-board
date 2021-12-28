a1022c862ae37d5e4a0ed5889ad4257c303943dd
carmineding <carmineding@tencent.com>
2021-12-28 18:05:31 +0800

    add flex layout, show time from now in page

--- message_board/views.py
+++ message_board/views.py
@@ -2,35 +2,46 @@ from django.shortcuts import render
 from .forms import MessageForm
 from .models import Message
 from django.http import HttpResponse
+from django.utils.timezone import now
 
 
 # Create your views here.
 def index(request):
+    print(request.method)
     form = MessageForm(request.POST)
-    message_list = Message.objects.values()
-    print(message_list)
-    context = {
-        'form': form,
-        'message_list': message_list
-    }
+
     if request.method == 'POST':
         if form.is_valid():
             Message.objects.create(name=form.cleaned_data['name'], body=form.cleaned_data['body'])
+            message_list = list(Message.objects.values())
+            for message in message_list:
+                message['days_from_now'] = (now() - message['timestamp']).days
+                message['hours_from_now'] = (now() - message['timestamp']).seconds // 3600
+                message['minutes_from_now'] = ((now() - message['timestamp']).seconds // 60) % 60
             # reset form to blank
-            form = MessageForm()
+            # form = MessageForm()
             context = {
                 'form': form,
                 'message_list': message_list
             }
             return render(request, 'message_board/index.html', context)
-        else:
-            for value in form.errors.get_json_data().values():
-                error_message = value[0]['message']
-            context = {
-                'form': form,
-                'message_list': message_list,
-                'error_message': error_message
-            }
-            return render(request, 'message_board/index.html', context)
-
-    return render(request, 'message_board/index.html', context)
+        # else:
+        #     for value in form.errors.get_json_data().values():
+        #         error_message = value[0]['message']
+        #     context = {
+        #         'form': form,
+        #         'message_list': message_list,
+        #         # 'error_message': error_message
+        #     }
+        #     return render(request, 'message_board/index.html', context)
+    elif request.method == 'GET':
+        message_list = list(Message.objects.values())
+        for message in message_list:
+            message['days_from_now'] = (now() - message['timestamp']).days
+            message['hours_from_now'] = (now() - message['timestamp']).seconds // 3600
+            message['minutes_from_now'] = ((now() - message['timestamp']).seconds // 60) % 60
+        context = {
+            'form': form,
+            'message_list': message_list
+        }
+        return render(request, 'message_board/index.html', context)
