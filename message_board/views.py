2ce6a3503d6b960b4323f7c049e4121a2608782a
carmineding <carmineding@tencent.com>
2021-12-29 17:53:02 +0800

    add layout

--- message_board/views.py
+++ message_board/views.py
@@ -13,12 +13,6 @@ def index(request):
     if request.method == 'POST':
         if form.is_valid():
             Message.objects.create(name=form.cleaned_data['name'], body=form.cleaned_data['body'])
-            message_list = list(Message.objects.values())
-            for message in message_list:
-                message['days_from_now'] = (now() - message['timestamp']).days
-                message['hours_from_now'] = (now() - message['timestamp']).seconds // 3600
-                message['minutes_from_now'] = ((now() - message['timestamp']).seconds // 60) % 60
-            # always return HttpResponseRedirect after successfully dealing with POST data
             return HttpResponseRedirect('/')
         else:
             for value in form.errors.get_json_data().values():
@@ -32,6 +26,7 @@ def index(request):
             message['minutes_from_now'] = ((now() - message['timestamp']).seconds // 60) % 60
         context = {
             'form': form,
-            'message_list': message_list
+            'message_list': message_list,
+            'index': len(message_list)
         }
         return render(request, 'message_board/index.html', context)
