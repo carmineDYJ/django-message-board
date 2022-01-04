ae4945cbb28d6af5fd96ac2e2f0cc47961474e21
carmineding <carmineding@tencent.com>
2022-01-04 17:04:42 +0800

    change form style

--- message_board/forms.py
+++ message_board/forms.py
@@ -3,8 +3,8 @@ from django.core.exceptions import ValidationError
 
 
 class MessageForm(forms.Form):
-    name = forms.CharField(label='name', max_length=20, min_length=1, required=True)
-    body = forms.CharField(label='message', max_length=200,  min_length=1, required=True, widget=forms.Textarea)
+    name = forms.CharField(label='Name', max_length=20, min_length=1, required=True)
+    body = forms.CharField(label='Message', max_length=200,  min_length=1, required=True, widget=forms.Textarea)
     # def clean_name(self):
     #     name = self.cleaned_data['name']
     #     name = name.strip()
